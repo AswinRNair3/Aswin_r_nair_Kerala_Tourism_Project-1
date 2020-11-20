919a6ce8db92680eb7d2b83d99604febc6eaa56f
AswinRNair3 <aswinkrishh007@gmail.com>
2020-11-20 18:51:56 +0530

    First commit:First Version

---
+++ script.js
@@ -0,0 +1,42 @@
+let phno= document.getElementById("phno");
+let pwd= document.getElementById("pwd");
+let cpwd= document.getElementById("cpwd");
+let strength= document.getElementById("strength");
+//  let regemail=/^([A-Za-z0-9\.]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/ Used email as the type.
+  
+
+
+function validate1()
+{
+  
+ let regp=/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
+ 
+  if(regp.test(phno.value) )
+  {
+   return true;
+  }
+  else
+  { alert("Enter Phone number in correct format.")
+    return false;
+   
+  }
+  
+}
+
+function pass()
+{let regpwd=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/
+  if(regpwd.test(pwd.value))
+  {strength.innerHTML="Success";
+  strength.style.color="green";
+  strength.style.border="2px solid green";
+   return true;
+  }
+  else
+  {strength.innerHTML="Password must contain minimum 8 characters, atleast 1 uppercase,1 lowercaseand a number";
+  strength.style.color="red";
+  strength.style.border="2px solid red";
+   return true;
+  }
+}
+
+
